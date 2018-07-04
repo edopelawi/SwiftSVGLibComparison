76fc286789f885fcf35758975cff06a2d8dda7fd
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2018-07-04 11:37:44 +0700

    Initial commit for URLConstants

---
+++ SVGPlayground/URLConstants.swift
@@ -0,0 +1,13 @@
+//
+//  URLConstants.swift
+//  SVGPlayground
+//
+//  Created by Ricardo Pramana Suranta on 04/07/18.
+//  Copyright © 2018 Ricardo Pramana Suranta. All rights reserved.
+//
+
+import Foundation
+
+enum URLConstants {
+	static let sampleSVGPath = "https://a.m.dana.id/resource/imgs/entrance/insurance.svg"
+}
