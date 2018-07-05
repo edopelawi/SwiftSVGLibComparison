d06b6af24d0a89de4c816b587d10f30b02a7aa15
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2018-07-05 10:18:28 +0700

    Initial commit for MacawSVGCell

---
+++ SVGPlayground/TableViewCells/MacawSVGCell.swift
@@ -0,0 +1,28 @@
+//
+//  MacawSVGCell.swift
+//  SVGPlayground
+//
+//  Created by Ricardo Pramana Suranta on 05/07/18.
+//  Copyright © 2018 Ricardo Pramana Suranta. All rights reserved.
+//
+
+import UIKit
+import Macaw
+
+final class MacawSVGCell: UITableViewCell {
+
+	static let preferredHeight: CGFloat = 70.0
+	static let reuseIdentifier: String = "MacawSVGCell"
+
+	@IBOutlet private weak var macawSVGView: Macaw.SVGView!
+
+	@IBOutlet private weak var explanationLabel: UILabel!
+
+	/**
+	Configures this cell to display the passed `filename`, without the file format (`.svg`).
+	*/
+	func configure(for filename: String) {
+		explanationLabel.text = "Macaw: \(filename)"
+		macawSVGView.fileName = filename
+	}
+}
