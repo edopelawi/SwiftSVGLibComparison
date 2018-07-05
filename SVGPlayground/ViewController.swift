362c7567d4cfe49e87de23f120a8c05146577b87
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2018-07-05 10:40:51 +0700

    Initial attempt to replace ViewController's sample label and view to table view
    This makes performance testing easier.

--- SVGPlayground/ViewController.swift
+++ SVGPlayground/ViewController.swift
@@ -10,23 +10,121 @@ import UIKit
 import SwiftSVG
 import Macaw
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
-	@IBOutlet private weak var swiftSVGView: SwiftSVG.SVGView!
-	@IBOutlet private weak var macawSVGView: Macaw.SVGView!
+	@IBOutlet private weak var tableView: UITableView!
+
+	enum Section: Int {
+		case macaw = 0
+		case swiftSVG
+	}
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		loadSVG()
+		configureTableView()
 	}
 
-	private func loadSVG() {
+	private func configureTableView() {
 
-		// TODO: Somehow, this part due to SVGView's rendering process. Try to uncomment this line when we changed the library version.
-		// swiftSVGView.SVGName = "insurance"
+		let macawNib = UINib(nibName: MacawSVGCell.reuseIdentifier, bundle: Bundle.main)
+		let swiftSVGNib = UINib(nibName: SwiftSVGCell.reuseIdentifier, bundle: Bundle.main)
 
-		macawSVGView.fileName = "insurance"
+		tableView.register(macawNib, forCellReuseIdentifier: MacawSVGCell.reuseIdentifier)
+		tableView.register(swiftSVGNib, forCellReuseIdentifier: SwiftSVGCell.reuseIdentifier)
 	}
 
 }
 
+// MARK: - UITableViewDataSource
+
+extension ViewController: UITableViewDataSource {
+
+	func numberOfSections(in tableView: UITableView) -> Int {
+		return 2
+	}
+
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+		guard let validSection = Section(rawValue: section) else {
+			return 0
+		}
+
+		switch validSection {
+		case .macaw:
+			return 1
+
+		case .swiftSVG:
+			return 0 // TODO: There's an issue in SwiftSVG rendering, so we're not using it for the time being.
+		}
+	}
+
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+		guard let reuseIdentifier = cellIdentifier(for: indexPath) else {
+			return UITableViewCell()
+		}
+
+		return tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+	}
+
+	private func cellIdentifier(for indexPath: IndexPath) -> String? {
+
+		guard let validSection = Section(rawValue: indexPath.section) else {
+			return nil
+		}
+
+		switch validSection {
+		case .macaw:
+			return MacawSVGCell.reuseIdentifier
+		case .swiftSVG:
+			return SwiftSVGCell.reuseIdentifier
+		}
+	}
+
+	func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+
+		guard let validSection = Section(rawValue: indexPath.section) else {
+			return 1.1
+		}
+
+		switch validSection {
+		case .macaw:
+			return MacawSVGCell.preferredHeight
+
+		case .swiftSVG:
+			return SwiftSVGCell.preferredHeight
+		}
+	}
+}
+
+// MARK: - UITableViewDelegate
+
+extension ViewController: UITableViewDelegate {
+
+	func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+
+		guard let validSection = Section(rawValue: section) else {
+			return nil
+		}
+
+		switch validSection {
+		case .macaw:
+			return "Macaw"
+
+		case .swiftSVG:
+			return "Swift SVG"
+		}
+	}
+
+	func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+
+		// TODO: We could make this better by making a protocol out of it.
+
+		if let macawCell = cell as? MacawSVGCell {
+			macawCell.configure(for: "insurance")
+		} else if let swiftSVGCell = cell as? SwiftSVGCell {
+			swiftSVGCell.configure(for: "insurance")
+		}
+	}
+
+}
